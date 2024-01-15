e5381658139d66072c8a8d6b41b83859c5663545
Jairo <jairo.espinoza@alarcongroup.us>
2024-01-15 15:38:03 -0500

    add experiencia laboral

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -5,16 +5,64 @@ import { ProjectsModel } from '../../models/Projects';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { featherAirplay } from '@ng-icons/feather-icons';
 import { heroUsers } from '@ng-icons/heroicons/outline';
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
 
+interface EventItem {
+  status?: string;
+  date?: string;
+  description?: string;
+  color?: string;
+  puesto?: string;
+}
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, TimelineModule, CardModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
   viewProviders: [provideIcons({ featherAirplay, heroUsers })]
 })
 export class ProjectsComponent {
+  events: EventItem[];
+
+  constructor() {
+    this.events = [
+      {
+        status: 'Medifarma',
+        date: 'Dic. 2017 – En. 2018 | Dic. 2018 – En. 2019',
+        description: 'Contabilizar el movimiento y almacenamiento de materias primas, productos (acabados o inacabados), herramientas y equipos.',
+        color: '#9C27B0',
+        puesto: 'Contable de inventario',
+      },
+      {
+        status: 'ConsigueVentas E.I.R.L',
+        date: 'Oct. 2021 – Ene. 2022',
+        description: `A cargo del equipo de desarrollo de Apis para el sistema de asistencia y Sistema ERP de la empresa, despliegue de proyectos en Hostinger, manejo de versiones en Github, documentación de Apis.`,
+        color: '#673AB7',
+        puesto: ' Programador Backend',
+      },
+      {
+        status: 'MDN Hatun S.A.C',
+        date: 'Abril 2022 – Enero 2023',
+        description: `• Desarrollo de la aplicación web CCAKU (Plataforma de transporte de maquinaria pesada).
+                      • Desarrollo de la aplicación web LARA.
+                      • Resolver requerimientos solicitados en el desarrollo de las plataformas, manejo de base de datos y despliegue en producción.`,
+        color: '#3F51B5',
+        puesto: 'Programador FullStack',
+      },
+      {
+        status: 'Alarcon Management Group AMG',
+        date: 'Enero 2023 – Diciembre 2023',
+        description: `• Integración de Tecnologías
+                      • Desarrollo y Mantenimiento del Software Interno
+                      • Colaboración y Trabajo en Equipo
+                      • Integración de herramientas externas y servicios para mejorar la funcionalidad del software interno`,
+        color: '#2196F3',
+        puesto: 'Software Engineer',
+      }
+    ];
+  }
   projectsListArray: ProjectsModel[] = [{
     "id": 1,
     "nombre": "IMMEQ",
